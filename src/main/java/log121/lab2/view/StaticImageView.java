db631484364e3f0ad3b8af5d813985b2da0af838
Guillaume Langlois <guillaume.langlois.dev@gmail.com>
2022-11-28 14:10:06 -0500

    Change to interface cause of stupid java

---
+++ src/main/java/log121/lab2/view/StaticImageView.java
@@ -0,0 +1,15 @@
+package log121.lab2.view;
+
+import log121.lab2.controller.Command;
+
+import java.util.List;
+
+public class StaticImageView extends ImageView{
+
+    private List<Command> commands;
+
+    public StaticImageView(List<Command> commands) {
+        super(commands);
+    }
+
+}
