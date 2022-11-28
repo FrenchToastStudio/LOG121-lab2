63bc95348a2c933359afb2959d01d7e7438316fc
Sergiu Cristian Ifimov <sergiu-cristian.ifimov.1@ens.etsmtl.ca>
2022-11-28 14:50:59 -0500

    ModificationController

--- src/main/java/log121/lab2/controller/ImageController.java
+++ src/main/java/log121/lab2/controller/ImageController.java
@@ -1,5 +1,15 @@
 package log121.lab2.controller;
 
+import log121.lab2.model.Image;
+
 public class ImageController {
     private Image image;
+
+    public ImageController(Image image) {
+        this.image = image;
+    }
+
+    public void loadImage(){
+
+    }
 }
--- src/main/java/log121/lab2/controller/ModificationController.java
+++ src/main/java/log121/lab2/controller/ModificationController.java
@@ -1,6 +1,29 @@
 package log121.lab2.controller;
 
+import log121.lab2.model.Image;
+
 public class ModificationController extends ImageController{
 
 
+    public ModificationController(Image image) {
+        super(image);
+    }
+
+    // This method allows for translation on the image with the provided parameters.
+    public void translate(int x, int y){
+
+    }
+
+    // This method allows for a zoom to occur on the image with the providede percentage.
+    public void zoom(int percentage){
+
+    }
+
+    public void undo(){
+
+    }
+
+    private void createMemento(){
+
+    }
 }
