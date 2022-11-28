040cc1780501427c299d777c50cca2d3f10af1ef
Karl-Eric Jolicoeur <karlaujo168@gmail.com>
2022-11-28 14:49:27 -0500

    controllers

---
+++ src/main/java/log121/lab2/controller/MainController.java
@@ -0,0 +1,16 @@
+package log121.lab2.controller;
+
+import log121.lab2.model.Image;
+import log121.lab2.view.ImageView;
+
+import java.util.List;
+
+public class MainController {
+    private Image image;
+    private List<ImageView> views;
+    private int activeView;
+
+    public void changeView(){
+
+    }
+}
---
+++ src/main/java/log121/lab2/controller/OptionController.java
@@ -0,0 +1,7 @@
+package log121.lab2.controller;
+
+public class OptionController {
+    public void loadView(){}
+
+    public void saveImage(){}
+}
