3ab319262bc35691e59f609b2cbe4d5928fefb66
Sergiu Cristian Ifimov <sergiu-cristian.ifimov.1@ens.etsmtl.ca>
2022-11-28 15:02:36 -0500

    Modification Controller

--- src/main/java/log121/lab2/controller/ModificationController.java
+++ src/main/java/log121/lab2/controller/ModificationController.java
@@ -1,6 +1,9 @@
 package log121.lab2.controller;
 
 import log121.lab2.model.Image;
+import log121.lab2.model.Perspective;
+import log121.lab2.model.Position;
+import log121.lab2.model.Store;
 
 public class ModificationController extends ImageController{
 
@@ -11,16 +14,25 @@ public class ModificationController extends ImageController{
 
     // This method allows for translation on the image with the provided parameters.
     public void translate(int x, int y){
+        Position position = new Position();
+        position.setPosition(x,y);
 
+        Perspective perspective = new Perspective();
+        perspective.setPosition(position);
+
+        Store.getInstance().addPerspective(perspective);
     }
 
-    // This method allows for a zoom to occur on the image with the providede percentage.
+    // This method allows for a zoom to occur on the image with the provided percentage.
     public void zoom(int percentage){
+        Perspective perspective = new Perspective();
+        perspective.setZoom(percentage);
 
+        Store.getInstance().addPerspective(perspective);
     }
 
     public void undo(){
-
+    
     }
 
     private void createMemento(){
